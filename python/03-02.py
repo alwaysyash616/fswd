e7bfeafb264b867a8d1ccd3796e9764034b5db1a
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:53:57 +0530

    add A03-02 python

---
+++ python/03-02.py
@@ -0,0 +1,14 @@
+"""
+Assignment: 03
+Problem: 02
+Topic: Simple problems on print
+Description: Write a python script to print MySirG on the first line and Education Services on the second line.
+Date: 23-06-2026
+"""
+
+# print("MySirG")
+# print("Education Services")
+
+# print("MySirG\nEducation Services")
+
+print("MySirG\nEducation","Services")
