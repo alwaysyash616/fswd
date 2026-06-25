7a17312b3b19bb903dccc4a6a27e0bbadb5485b4
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:49:40 +0530

    add A02-02 python

---
+++ python/02-02.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 02
+Problem: 02
+Topic: Fundamentals of Python
+Description: What is PVM?
+Date: 23-06-2026
+"""
+
+"""
+Python Virtual Machine
+It's job is to translate byte code into OS dependent machine code.
+It also provide memory manager which manages program's memory during execution.
+"""
