7eab72d343e9e8acba1dd2dd905716ed527f7b04
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:48:44 +0530

    add A02-01 python

---
+++ python/02-01.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 02
+Problem: 01
+Topic: Fundamentals of Python
+Description: Write a command to get the Python version you are using.
+Date: 23-06-2026
+"""
+
+# python --version (on Windows)
+# python3 --version (on Mac/Linux) 
