45e70ee648033de04d975d8fe3648c6f9178579e
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:56:44 +0530

    add A03-05 python

---
+++ python/03-05.py
@@ -0,0 +1,15 @@
+"""
+Assignment: 03
+Problem: 05
+Topic: Simple problems on print
+Description: Write a python script to print \n on the screen.
+Date: 23-06-2026
+"""
+
+# print("\\n")
+
+# print('\\n')
+
+# print('''\\n''')
+
+print("""\\n""")
