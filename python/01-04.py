8973a83d4fc63ffc58b173ea196ced68371aaa40
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:46:07 +0530

    add A01-04 python

---
+++ python/01-04.py
@@ -0,0 +1,9 @@
+"""
+Assignment: 01
+Problem: 04
+Topic: Warmup
+Description: How to make single line comment in a python file?
+Date: 23-06-2026
+"""
+
+# This a single line comment in a python file.
