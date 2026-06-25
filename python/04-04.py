b077e08a7ded865935f9c112e7ce705581acf848
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:59:34 +0530

    add A04-04 python

---
+++ python/04-04.py
@@ -0,0 +1,21 @@
+"""
+Assignment: 04
+Problem: 04
+Topic: playing with variables
+Description: Create 5 variables each of them containing different types of data (like 35, True, "MySirG", 5.46, 3+4j, etc). Write a python script to print the values of all the variables along with their datatypes.
+Date: 23-06-2026
+"""
+
+# a=35
+# b=True
+# c="MySirG"
+# d=5.46
+# e=3+4j
+
+a,b,c,d,e=35,True,"MySirG",5.46,3+4j
+
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+print(e,type(e))
