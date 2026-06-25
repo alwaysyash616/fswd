a74e1d03eb7b33c0c2e3de35958235241d52e9f1
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:55:55 +0530

    add A03-04 python

---
+++ python/03-04.py
@@ -0,0 +1,15 @@
+"""
+Assignment: 03
+Problem: 04
+Topic: Simple problems on print
+Description: Write a python script to print "Teacher's Day" on the screen. (Remember to print single quote and double quote)
+Date: 23-06-2026
+"""
+
+# print("\"Teacher's Day\"")
+
+# print('''"Teacher's Day"''')
+
+# print('"Teacher\'s Day"')
+
+print("""\"Teacher's Day\"""")
