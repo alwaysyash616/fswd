20eb45771ca9e23021992b87b1ea12a18846b49e
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:55:34 +0530

    add A03-03 python

---
+++ python/03-03.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 03
+Problem: 03
+Topic: Simple problems on print
+Description: Write a python script to print "MySirG" on the screen. (Remember to print double quotes)
+Date: 23-06-2026
+"""
+
+# print("\"MySirG\"")
+
+# print('''"MySirG"''')
+
+print('"MySirG"')
