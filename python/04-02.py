1fc9e3bfebfe7e40bf359b2ea4ac73c39e3a6fd0
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:58:44 +0530

    add A04-02 python

---
+++ python/04-02.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 04
+Problem: 02
+Topic: playing with variables
+Description: Write a python script to print the value of a variable. Variable contains your name as data.
+Date: 23-06-2026
+"""
+
+name="Yash Kushwaha"
+print(name)
