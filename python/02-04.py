3f4ccf1c549615b0beb9d5f1d3bf6e2bc8b0d680
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:51:14 +0530

    add A02-04 python

---
+++ python/02-04.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 02
+Problem: 04
+Topic: Fundamentals of Python
+Description: How to run a Python file from command prompt or terminal?
+Date: 23-06-2026
+"""
+
+# python filename.py
+# python3 filename.py
