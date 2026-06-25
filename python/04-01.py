8ce5318932428f8db455ede978a85810ae7b41fe
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:57:58 +0530

    add A04-01 python

---
+++ python/04-01.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 04
+Problem: 01
+Topic: playing with variables
+Description: Write a python script containing a variables with some integer value, print value of this variable.
+Date: 23-06-2026
+"""
+
+a=1008
+print(a)
