4e54ee0fa736dd17610831b474d5ed5b2d3b8389
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 12:00:09 +0530

    add A04-05 python

---
+++ python/04-05.py
@@ -0,0 +1,14 @@
+"""
+Assignment: 04
+Problem: 05
+Topic: playing with variables
+Description: Create three variables and assign current date to them, first variable contains the day number, second variable contains the month number and third variable contains the year number. Write a python script to display date in standard way (e.g. 29/11/2022)
+Date: 23-06-2026
+"""
+
+# d=24
+# m=6
+# y=2026
+
+d,m,y=24,6,2026
+print(d,m,y,sep='/')
