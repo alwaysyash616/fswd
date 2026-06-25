5c1ec45b3e9659f5a8675130192741219e2445d4
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:59:07 +0530

    add A04-03 python

---
+++ python/04-03.py
@@ -0,0 +1,17 @@
+"""
+Assignment: 04
+Problem: 03
+Topic: playing with variables
+Description: Write a python script to print values of three variables, each in a new line. All three variables are filled with some integer values.
+Date: 23-06-2026
+"""
+
+# a=10
+# b=20
+# c=30
+# print(a)
+# print(b)
+# print(c)
+
+a,b,c=10,20,30
+print(a,b,c,sep='\n')
