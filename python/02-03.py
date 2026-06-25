d7de8f982661cfeef22487130f87ba5b94654b18
Yash Kushwaha <yash274602@gmail.com>
2026-06-25 11:50:16 +0530

    add A02-03 python

---
+++ python/02-03.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 02
+Problem: 03
+Topic: Fundamentals of Python
+Description: How to create .pyc (Byte Code) file from python source file?
+Date: 23-06-2026
+"""
+
+# python3 -m filename.py compile
+# python3 -m py_compile filename.py
