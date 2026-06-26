d43c09244ede2f6d4cb2bdd19473e609f5d988dd
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:32:38 +0530

    add A07-05 python

---
+++ python/07-05.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 07
+Problem: 05
+Topic: Number System
+Description: Write a python script to add two numbers 25(in octal) and 39(in hexadecimal) and display the result in binary format.
+Date: 25-06-2026
+"""
+
+a=0o25
+b=0x39
+c=a+b
+print(bin(c))
