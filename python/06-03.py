226d4a8a28dfa23bfb4dc4ced216296c9d2166b0
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:28:08 +0530

    add A06-03 python

---
+++ python/06-03.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 05
+Problem: 03
+Topic: Type Conversion
+Description: Write a python script to print character representation of given unicode 100.
+Date: 25-06-2026
+"""
+
+character=chr(100)
+print('Character corresponding to Unicode 100 is %s'%(character))
