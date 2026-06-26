5a677197eb7a4a8906bdae3680942bded042b7c3
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:35:17 +0530

    add A08-03 python

---
+++ python/08-03.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 08
+Problem: 03
+Topic: Input from keyboard
+Description: Write a python script which takes the radius from the user and display area of the circle.
+Date: 25-06-2026
+"""
+
+r=float(input("""Enter radius of circle: """))
+area=3.14*r*r
+print("Area is %f"%(area))
