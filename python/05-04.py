452cfb8e752129d30a6cb150888a3179b6ac5b25
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:25:34 +0530

    add A05-04 python

---
+++ python/05-04.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 05
+Problem: 04
+Topic: import and keywords
+Description: Out of all the keywords, Name those keywords which are used as data.
+Date: 23-06-2026
+"""
+
+"""
+True
+False
+None
+"""
