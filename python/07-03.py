5a79ad476f027c35073428f73987d74f662fef8f
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:31:40 +0530

    add A07-03 python

---
+++ python/07-03.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 07
+Problem: 03
+Topic: Number System
+Description: Write a python script to store a hexadecimal number 2F in a variable and print it in octal format.
+Date: 25-06-2026
+"""
+
+num=0x2F
+print(oct(num))
