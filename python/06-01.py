84f3ce32007be5730a84158bfd8ba8700a5d6b7b
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:27:12 +0530

    add A06-01 python

---
+++ python/06-01.py
@@ -0,0 +1,15 @@
+"""
+Assignment: 06
+Problem: 01
+Topic: Type Conversion
+Description: Write a python script to convert a number into str type.
+Date: 25-06-2026
+"""
+
+# str1=str(25)
+# print(str1)
+
+x=25
+print(x,type(x))
+x=str(x)
+print(x,type(x))
