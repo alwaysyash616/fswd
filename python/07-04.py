9bd4158b1b39256dc1a579901dbb0225605fd943
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:32:20 +0530

    add A07-04 python

---
+++ python/07-04.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 07
+Problem: 04
+Topic: Number System
+Description: Write a python script to store an octal number 125 in a variable and print it in binary format.
+Date: 25-06-2026
+"""
+
+num=0o125
+print(bin(num))
