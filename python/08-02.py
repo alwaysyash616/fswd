e483345ffc65258bff852f0792e32c4ae07316f8
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:34:52 +0530

    add A08-02 python

---
+++ python/08-02.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 08
+Problem: 02
+Topic: Input from keyboard
+Description: Write a python script to take input two numbers from the user, then calculate their sum and display the result.
+Date: 25-06-2026
+"""
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=a+b
+print("Sum is %d"%(c))
