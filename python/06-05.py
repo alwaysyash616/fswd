9a86006137f6506aad1b0490e3caf6fc7b3fcd41
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:29:02 +0530

    add A06-05 python

---
+++ python/06-05.py
@@ -0,0 +1,15 @@
+"""
+Assignment: 05
+Problem: 05
+Topic: Type Conversion
+Description: How to convert an integer value into a bool value?
+Date: 25-06-2026
+"""
+
+a,b=34,0
+c,d=bool(a),bool(b)
+print(c,d)
+
+"""
+It should be passed into the bool conversion function.
+"""
