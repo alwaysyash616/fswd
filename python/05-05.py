05dce8a912e256cff0cf5198b2ef4a89cd744c0f
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:26:02 +0530

    add A05-05 python

---
+++ python/05-05.py
@@ -0,0 +1,9 @@
+"""
+Assignment: 05
+Problem: 05
+Topic: import and keywords
+Description: What is the use of del keyword
+Date: 23-06-2026
+"""
+
+# to remove (release memory of) a reference variable in namespace stack
