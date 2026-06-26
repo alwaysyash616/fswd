b4df119b6d496d7904c3a0ff41eb7ee9752df8ca
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:35:59 +0530

    add A08-05 python

---
+++ python/08-05.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 08
+Problem: 05
+Topic: Input from keyboard
+Description: Write a python script which takes a character from the user and display its unicode.
+Date: 25-06-2026
+"""
+
+character=input("Enter a character: ")
+print("Unicode for the character",character,"is",ord(character))
