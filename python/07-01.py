0c787fa806d88ff8ffc2f183d81264f5ab233bd0
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:30:44 +0530

    add A07-01 python

---
+++ python/07-01.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 07
+Problem: 01
+Topic: Number System
+Description: Write a python script to print any number and it's binary equivalent.
+Date: 25-06-2026
+"""
+
+num=25
+print(num,bin(num))
