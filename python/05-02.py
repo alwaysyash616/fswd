344bffc2f5aeebb3299575fa37c7da061c7ead35
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:23:57 +0530

    add A05-02 python

---
+++ python/05-02.py
@@ -0,0 +1,18 @@
+"""
+Assignment: 05
+Problem: 02
+Topic: import and keywords
+Description: Use help section on python shell to print all the keywords.
+Date: 23-06-2026
+"""
+
+"""
+python3
+help()
+keywords
+"""
+
+"""
+python3
+help('keywords')
+"""
