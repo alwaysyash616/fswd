f642900afcb72841faea4d2b9efd935c68d76fae
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:35:42 +0530

    add A08-04 python

---
+++ python/08-04.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 08
+Problem: 04
+Topic: Input from keyboard
+Description: Write a python script to calculate square of a number. Number is entered by the user.
+Date: 25-06-2026
+"""
+
+num=int(input("Enter a number: "))
+print("Square of",num,"is",num*num)
