b8bc24ef0dbe9f7c5f01fc507f654aa3961d5dc7
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:34:33 +0530

    add A08-01 python

---
+++ python/08-01.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 08
+Problem: 01
+Topic: Input from keyboard
+Description: Write a python script to take your name as input from the user and then print it.
+Date: 25-06-2026
+"""
+
+name=input('Enter your name: ')
+print(name)
