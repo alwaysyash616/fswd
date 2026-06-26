7bd0f5e8df19e3b8da4e06e118c1d4a966e137ed
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:25:01 +0530

    add A05-03 python

---
+++ python/05-03.py
@@ -0,0 +1,24 @@
+"""
+Assignment: 05
+Problem: 03
+Topic: import and keywords
+Description: Create two Python files A0.py and A1.py. Create a variable in A1.py and assign some value to it. Write a python script to import A1 module in A0 and print value of the variable created in A0.py
+Date: 23-06-2026
+"""
+
+"""
+A1.py
+username="alwaysyash"
+
+A0.py
+---------------------
+import A1
+print(A1.username)
+---------------------
+from A1 import username
+print(username)
+---------------------
+from A1 import username as name
+print(name)
+
+"""
