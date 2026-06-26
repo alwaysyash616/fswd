e93fe2ce4af9e14dd0c432812e9b350c37505dd6
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:27:41 +0530

    add A06-02 python

---
+++ python/06-02.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 05
+Problem: 02
+Topic: Type Conversion
+Description: Write a python script to print Unicode of the character 'm'
+Date: 25-06-2026
+"""
+
+x=ord('m')
+print('''Unicode of \'m\' is''',x)
