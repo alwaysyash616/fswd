23bfa719b67a05335badb6efd18155803d151345
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:31:10 +0530

    add A07-02 python

---
+++ python/07-02.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 07
+Problem: 02
+Topic: Number System
+Description: Write a python script to store binary number 1100101 in a variable and print it in decimal format.
+Date: 25-06-2026
+"""
+
+num=0b1100101
+print(num)
