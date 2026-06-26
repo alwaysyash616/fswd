53abcacc8acf68c11c2e19a4a2c61e5dee25a43c
Yash Kushwaha <yash274602@gmail.com>
2026-06-26 10:22:30 +0530

    add A05-01 python

---
+++ python/05-01.py
@@ -0,0 +1,20 @@
+"""
+Assignment: 05
+Problem: 01
+Topic: import and keywords
+Description: Write a python script to print all keywords on the screen.
+Date: 23-06-2026
+"""
+
+import keyword
+print(keyword.kwlist)
+
+"""
+from keyword import kwlist
+print(kwlist)
+"""
+
+"""
+from keyword import kwlist as keywords
+print(keywords)
+"""
