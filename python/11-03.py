233303b1ecdd8d68e98a26118f5c55a62b93f71b
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:40:56 +0530

    add A11-03 python

---
+++ python/11-03.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 11
+Problem: 03
+Topic: More on Operators
+Description: What will be the output of the python expression 5>10<5 ?
+Date: 25-06-2026
+"""
+
+print(5>10<5)
+
+"""
+False
+"""
