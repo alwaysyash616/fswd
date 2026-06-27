6d376cf4dfdd5966ef9088747fac534f1192c4ee
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:40:33 +0530

    add A11-02 python

---
+++ python/11-02.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 11
+Problem: 02
+Topic: More on Operators
+Description: Write a python script to input two strings from the user and display wheather the two variables referred to the same object or not. Print True or False.
+Date: 25-06-2026
+"""
+
+a=input("Enter first string: ")
+b=input("Enter second string: ")
+print(a is b)
