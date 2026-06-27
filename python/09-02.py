c464f9213538e19428096f5f97231944ae21c41e
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:35:50 +0530

    add A09-02 python

---
+++ python/09-02.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 09
+Problem: 02
+Topic: Simple calculations on user data
+Description: Write a python script to calculate area of rectangle.
+Date: 25-06-2026
+"""
+
+l=int(input("Enter length: "))
+b=int(input("Enter breadth: "))
+area=l*b
+print("Area of rectangle: ",area)
