4e3e1169f0bf3ad2d5c25fe7c4d6e0a1be279027
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:39:14 +0530

    add A10-04 python

---
+++ python/10-04.py
@@ -0,0 +1,15 @@
+"""
+Assignment: 10
+Problem: 04
+Topic: Operators
+Description: Write a python script which takes a 3 digit number from the user and displays only its first digit.
+Date: 25-06-2026
+"""
+
+# num=int(input("Enter a three digit number: "))
+# num//=10
+# num//=10
+# print("First digit: %d"%(num))
+
+num=int(input("Enter a three digit number: "))
+print("First digit:",num//100)
