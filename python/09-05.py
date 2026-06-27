50a9441c9765d7a4a9cc30c5195117d557f04ac9
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:37:15 +0530

    add A09-05 python

---
+++ python/09-05.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 09
+Problem: 05
+Topic: Simple calculations on user data
+Description: Write a python script to take two numbers from the user (say x and y), now calculate x raised to the power y and display the result.
+Date: 25-06-2026
+"""
+
+x=int(input("Enter a number: "))
+y=int(input("Enter another number: "))
+z=x**y
+print(z)
