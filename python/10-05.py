3571afa25d9f3322311fa55c80bfffa2f0811b41
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:39:37 +0530

    add A10-05 python

---
+++ python/10-05.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 10
+Problem: 05
+Topic: Operators
+Description: Write a python script which takes a 3 digit number from the user and displays only its middle digit.
+Date: 25-06-2026
+"""
+
+num=int(input("Enter a three digit number: "))
+print("Middle digit: ",num//10%10)
