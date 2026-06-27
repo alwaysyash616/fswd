14ecda95bf527c2f5944ad2cfc9edc4916ae27df
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:38:12 +0530

    add A10-02 python

---
+++ python/10-02.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 10
+Problem: 02
+Topic: Operators
+Description: Write a python script to get the last digit from a given number. (for example, if user enters 2089 then your output should be 9)
+Date: 25-06-2026
+"""
+
+num=int(input("Enter a number: "))
+res=num%10
+print(res)
