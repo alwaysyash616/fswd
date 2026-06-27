e18618724d2b8fb19dfe4a2782ed2aab463dde25
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:39:58 +0530

    add A11-01 python

---
+++ python/11-01.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 11
+Problem: 01
+Topic: More on Operators
+Description: Write a python script to print True if the string 'my' is a member in a string entered by user.
+Date: 25-06-2026
+"""
+
+string=input("Enter a string literal: ")
+print('my' in string)
