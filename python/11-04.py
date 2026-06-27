e485a5b40bee54a3fa524f633d304095cd2b3f79
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:41:49 +0530

    add A11-04 python

---
+++ python/11-04.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 11
+Problem: 04
+Topic: More on Operators
+Description: What will be the output of the python expression "Red" and "White" ?
+Date: 25-06-2026
+"""
+
+result="Red" and "White"
+print(result)
