e83727498357e58f37b9a9bf204660a470ca87d7
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:36:32 +0530

    add A09-04 python

---
+++ python/09-04.py
@@ -0,0 +1,15 @@
+"""
+Assignment: 09
+Problem: 04
+Topic: Simple calculations on user data
+Description: Write a python script to calculate volume of a cuboid.
+Date: 25-06-2026
+"""
+
+l=int(input("Enter length of cuboid: "))
+b=int(input("Enter breadth of cuboid: "))
+h=int(input("Enter height of cuboid: "))
+
+vol=l*b*h
+
+print("Volume of cuboid: ",vol)
