8d021d771292777360cb41458b798a3a29b45813
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:37:50 +0530

    add A10-01 python

---
+++ python/10-01.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 10
+Problem: 01
+Topic: Operators
+Description: Write a python script to remove the last digit from a given number. (for example, if user enters 2534 then your output should be 253)
+Date: 25-06-2026
+"""
+
+num=int(input("Enter a number: "))
+res=num//10
+print(res)
