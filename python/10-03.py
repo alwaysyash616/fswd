799e341f560720035819c6e877c9c0e7eabe15d5
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:38:34 +0530

    add A10-03 python

---
+++ python/10-03.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 10
+Problem: 03
+Topic: Operators
+Description: Write a python script to swap data of two variables.
+Date: 25-06-2026
+"""
+
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+print("a =",a,"b =",b)
+a,b=b,a
+print("a =",a,"b =",b)
