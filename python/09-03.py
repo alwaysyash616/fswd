b1819457b830aae511ce2c83c495461dc871d27d
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:36:10 +0530

    add A09-03 python

---
+++ python/09-03.py
@@ -0,0 +1,15 @@
+"""
+Assignment: 09
+Problem: 03
+Topic: Simple calculations on user data
+Description: Write a python script to calculate average of three numbers entered by user.
+Date: 25-06-2026
+"""
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+
+avg=(a+b+c)/3
+
+print("Average: ",avg)
