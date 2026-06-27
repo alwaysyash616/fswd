b271fd84d2cba0aaefc05e2e86fccb3b7b7c40f5
Yash Kushwaha <yash274602@gmail.com>
2026-06-27 17:42:32 +0530

    add A11-05 python

---
+++ python/11-05.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 11
+Problem: 05
+Topic: More on Operators
+Description: What will be the output of the python expression True or False ?
+Date: 25-06-2026
+"""
+
+print(True or False)
+
+"""
+True
+"""
