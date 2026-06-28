e86e06e79705964ffd8447d558897195817a98ae
Yash Kushwaha <yash274602@gmail.com>
2026-06-28 21:06:55 +0530

    add A12-01 python

---
+++ python/12-01.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 12
+Problem: 01
+Topic: Descision Control
+Description: Write a python script to check wheather a given number is positive or non-positive
+Date: 26-06-2026
+"""
+
+num=int(input("Enter a number: "))
+if num>0:
+    print("Positive number")
+else:
+    print("Non-Positive number")
