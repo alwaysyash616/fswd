8d1dd0bad7531f06cb24a14bbb7ccb6cecf6957d
Yash Kushwaha <yash274602@gmail.com>
2026-06-28 21:07:37 +0530

    add A12-03 python

---
+++ python/12-03.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 12
+Problem: 03
+Topic: Descision Control
+Description: Write a python script to check wheather a given number is even or odd
+Date: 26-06-2026
+"""
+
+num=int(input("Enter a number: "))
+if num%2:
+    print("Odd")
+else:
+    print("Even")
