d67d6a40ba4df6b7481b1f3db60a3f606c44b87c
Yash Kushwaha <yash274602@gmail.com>
2026-06-28 21:08:06 +0530

    add A12-04 python

---
+++ python/12-04.py
@@ -0,0 +1,14 @@
+"""
+Assignment: 12
+Problem: 04
+Topic: Descision Control
+Description: Write a python script to print greater between two numbers. Print number only once even if the numbers are the same.
+Date: 26-06-2026
+"""
+
+print("Enter two numbers:")
+a,b=int(input()),int(input())
+if a>b:
+    print(a)
+else:
+    print(b)
