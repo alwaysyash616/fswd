25993c1e9051f2770e3d68cb0eb2a10cf8c447c5
Yash Kushwaha <yash274602@gmail.com>
2026-06-28 21:13:35 +0530

    add A13-02 python

---
+++ python/13-02.py
@@ -0,0 +1,15 @@
+"""
+Assignment: 13
+Problem: 02
+Topic: More on Descision Control
+Description: Write a python script to check wheather a given number is positive, negative or zero.
+Date: 26-06-2026
+"""
+
+num=int(input("Enter a number: "))
+if num==0:
+    print("zero")
+elif num>0:
+    print("positive")
+else:
+    print("negative")
