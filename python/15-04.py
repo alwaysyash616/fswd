7456d8ec12618c913321134d1ccbe1c1b0adf0d7
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 17:58:53 +0530

    add A15-04 python

---
+++ python/15-04.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 15
+Problem: 04
+Topic: while loop
+Description: Write a python script to print first 10 odd natural numbers
+Date: 26-06-2026
+"""
+
+i=1
+while i<=10:
+    print(2*i-1)
+    i+=1
