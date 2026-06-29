6e6b4fe5bc510cf1a38d4971a3fafc34cfbcd52c
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 18:26:59 +0530

    add A16-03 python

---
+++ python/16-03.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 16
+Problem: 03
+Topic: more on while loop
+Description: Write a python script to print squares of first 10 natural numbers
+Date: 26-06-2026
+"""
+
+i=1
+while i<=10:
+    print(i**2)
+    i+=1
