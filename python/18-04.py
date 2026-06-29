e65f4db865fd88456e55c07f194d19834e6062e1
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 21:43:13 +0530

    add A18-04 python

---
+++ python/18-04.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 18
+Problem: 04
+Topic: more on while loop with user input
+Description: Write a python script to print cubes of first N natural numbers
+Date: 29-06-2026
+"""
+
+N=int(input("Enter a number: "))
+i=1
+while i<=N:
+    print(i**3)
+    i+=1
