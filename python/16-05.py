acbeb08b8caad54721538976f9981bfa2bcb565a
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 18:27:47 +0530

    add A16-05 python

---
+++ python/16-05.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 16
+Problem: 05
+Topic: more on while loop
+Description: Write a python script to print first 10 multiples of 5
+Date: 26-06-2026
+"""
+
+i=1
+while i<=10:
+    print(5*i)
+    i+=1
