0d9163dc7d718b7e12cc05641036eb462d42a680
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 21:42:03 +0530

    add A18-02 python

---
+++ python/18-02.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 18
+Problem: 02
+Topic: more on while loop with user input
+Description: Write a python script to print first N even natural numbers in reverse order
+Date: 29-06-2026
+"""
+
+N=int(input("Enter a number: "))
+while N>=1:
+    print(N*2)
+    N-=1
