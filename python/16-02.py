31fd4d02f376a57c186712d7200cdd50ff43a178
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 18:26:35 +0530

    add A16-02 python

---
+++ python/16-02.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 16
+Problem: 02
+Topic: more on while loop
+Description: Write a python script to print first 10 even natural numbers in reverse order
+Date: 26-06-2026
+"""
+
+i=10
+while i>=1:
+    print(2*i)
+    i-=1
