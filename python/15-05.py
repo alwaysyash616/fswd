64be8fb36cc9fc362d71a75387bceac6bd8fed2a
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 17:59:12 +0530

    add A15-05 python

---
+++ python/15-05.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 15
+Problem: 05
+Topic: while loop
+Description: Write a python script to print first 10 odd natural numbers in reverse order
+Date: 26-06-2026
+"""
+
+i=10
+while i>=1:
+    print(2*i-1)
+    i-=1
