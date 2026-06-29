fff221fee44ce9a3cf19325b36bc5398dd54d987
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 17:57:38 +0530

    add A15-01 python

---
+++ python/15-01.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 15
+Problem: 01
+Topic: while loop
+Description: Write a python script to print MySirG 5 times on the screen
+Date: 26-06-2026
+"""
+
+i=1
+while i<=5:
+    print("MySirG")
+    i+=1
