282b913f257b458073ea279b5301c01407afba49
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 21:42:29 +0530

    add A18-03 python

---
+++ python/18-03.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 18
+Problem: 03
+Topic: more on while loop with user input
+Description: Write a python script to print squares of first N natural numbers
+Date: 29-06-2026
+"""
+
+N=int(input("Enter a number: "))
+i=1
+while i<=N:
+    print(i**2)
+    i+=1
