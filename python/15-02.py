95425f06cc0f498a86324990eae8c93ef5033b69
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 17:57:59 +0530

    add A15-02 python

---
+++ python/15-02.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 15
+Problem: 02
+Topic: while loop
+Description: Write a python script to print first 10 natural numbers
+Date: 26-06-2026
+"""
+
+i=1
+while i<=10:
+    print(i)
+    i+=1
