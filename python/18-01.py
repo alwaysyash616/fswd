2b1ef305200b8f411d17ff6dbca8f4f25df876e8
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 21:41:41 +0530

    add A18-01 python

---
+++ python/18-01.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 18
+Problem: 01
+Topic: more on while loop with user input
+Description: Write a python script to print first N even natural numbers
+Date: 29-06-2026
+"""
+
+N=int(input("Enter a number: "))
+i=1
+while i<=N:
+    print(2*i)
+    i+=1
