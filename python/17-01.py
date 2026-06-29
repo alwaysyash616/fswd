93ed487ddbe017352c630da952b2d0495fd348b1
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 18:28:11 +0530

    add A17-01 python

---
+++ python/17-01.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 17
+Problem: 01
+Topic: while loop with user input
+Description: Write a python script to print MySirG N times on the screen
+Date: 26-06-2026
+"""
+
+N=int(input("Enter a number: "))
+i=1
+while i<=N:
+    print("MySirG")
+    i+=1
