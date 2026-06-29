eb64410e594b06327f746dbaabf5fed58be84c29
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 18:29:21 +0530

    add A17-04 python

---
+++ python/17-04.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 17
+Problem: 04
+Topic: while loop with user input
+Description: Write a python script to print first N odd natural numbers
+Date: 26-06-2026
+"""
+
+N=int(input("Enter a number: "))
+i=1
+while i<=N:
+    print(2*i-1)
+    i+=1
