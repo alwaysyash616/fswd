099f9211e92397c9ad9b591fa207f8395e607c50
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 18:28:36 +0530

    add A17-02 python

---
+++ python/17-02.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 17
+Problem: 02
+Topic: while loop with user input
+Description: Write a python script to print first N natural numbers
+Date: 26-06-2026
+"""
+
+N=int(input("Enter a number: "))
+i=1
+while i<=N:
+    print(i)
+    i+=1
