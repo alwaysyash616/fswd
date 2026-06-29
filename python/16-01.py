39509e3b39d292cb61ab1378eb433a34872897e6
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 18:25:34 +0530

    add A16-01 python

---
+++ python/16-01.py
@@ -0,0 +1,20 @@
+"""
+Assignment: 16
+Problem: 01
+Topic: more on while loop
+Description: Write a python script to print first 10 even natural numbers
+Date: 26-06-2026
+"""
+
+i=1
+while i<=10:
+    print(2*i)
+    i+=1
+
+"""
+1)  Read
+2)  Evaluate
+3)  Dry Run
+
+You dry run and find your mistake. If someone else tell you about your mistake then your improvement is't started. And if the computer tell you about your mistake (then it's the worst). The day you start finding your mistakes, that day will be starting of your improvement.
+"""
