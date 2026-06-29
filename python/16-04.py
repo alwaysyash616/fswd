290ac0eff23286ff9f41e60a3e7e927e92c19e60
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 18:27:24 +0530

    add A16-04 python

---
+++ python/16-04.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 16
+Problem: 04
+Topic: more on while loop
+Description: Write a python script to print cubes of first 10 natural numbers
+Date: 26-06-2026
+"""
+
+i=1
+while i<=10:
+    print(i**3)
+    i+=1
