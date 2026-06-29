0d984915eecdfd48b21d509d33653235082f833a
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 18:29:40 +0530

    add A17-05 python

---
+++ python/17-05.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 17
+Problem: 05
+Topic: while loop with user input
+Description: Write a python script to print first N odd natural numbers in reverse order
+Date: 26-06-2026
+"""
+
+N=int(input("Enter a number: "))
+while N>=1:
+    print(2*N-1)
+    N-=1
