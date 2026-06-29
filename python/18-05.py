729b2191ccd8964b7cce3223a1e6b02323554e05
Yash Kushwaha <yash274602@gmail.com>
2026-06-29 21:43:42 +0530

    add A18-05 python

---
+++ python/18-05.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 18
+Problem: 05
+Topic: more on while loop with user input
+Description: Write a python script to print first 10 multiples of N
+Date: 29-06-2026
+"""
+
+N=int(input("Enter a number: "))
+i=1
+while i<=10:
+    print(N*i)
+    i+=1
