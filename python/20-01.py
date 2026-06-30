4603bf1ddf692a20501475a7cf7058bb0349ee0d
Yash Kushwaha <yash274602@gmail.com>
2026-06-30 19:20:37 +0530

    add A20-01 python

---
+++ python/20-01.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 20
+Problem: 01
+Topic: for loop and range
+Description: Write a python script to print the first 10 multiples of 5.
+Date: 30-06-2026
+"""
+
+for x in range(5,51,5):
+    print(x,end=' ')
+print()
