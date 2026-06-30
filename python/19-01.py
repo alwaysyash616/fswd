da9d40567c6911f42bb0e5cf1bb1d2b5cd1f3634
Yash Kushwaha <yash274602@gmail.com>
2026-06-30 18:59:12 +0530

    add A19-01 python

---
+++ python/19-01.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 19
+Problem: 01
+Topic: for loop
+Description: Write a python script to print each character of a string with its corresponding Unicode.
+Date: 30-06-2026
+"""
+
+s='''Veo LMS'''
+for x in s:
+    print(x,ord(x))
