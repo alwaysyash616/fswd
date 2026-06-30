84cc5c786f6f9ebc550c2f4e6dfb2a391a5c2fef
Yash Kushwaha <yash274602@gmail.com>
2026-06-30 19:34:35 +0530

    add A21-03 python

---
+++ python/21-03.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 21
+Problem: 03
+Topic: more on for loop and range
+Description: Write a python script to print squares of first N natural numbers.
+Date: 30-06-2026
+"""
+
+n=int(input("Enter a number: "))
+for e in range(1,n+1):
+    print(e**2,end=' ')
+print()
