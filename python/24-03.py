11dd22edbce34273950975c60c858d06ac095c2f
Yash Kushwaha <yash274602@gmail.com>
2026-07-02 07:33:49 +0530

    add A24-03 python

---
+++ python/24-03.py
@@ -0,0 +1,17 @@
+"""
+Assignment: 24
+Problem: 03
+Topic: list
+Description: Write a python script to create a list of squares of numbers of a given list.
+Date: 30-06-2026
+"""
+
+l1=[2,5,6,9,10]
+print(l1)
+l2=[e**2 for e in l1]
+print(l2)
+
+# print("Enter numbers separated by comma")
+# l1=[int(e) for e in input().split(',')]
+# l2=[e**2 for e in l1]
+# print(l2)
