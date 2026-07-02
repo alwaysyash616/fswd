838ea3c2251443082f73251ef941cad52c0c654a
Yash Kushwaha <yash274602@gmail.com>
2026-07-02 09:09:49 +0530

    add A25-01 python

---
+++ python/25-01.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 25
+Problem: 01
+Topic: more on list
+Description: Write a python script to create a list of first N even natural numbers
+Date: 30-06-2026
+"""
+
+print("Enter a number:")
+l1=[2*e for e in range(1,int(input())+1)]
+print(l1)
