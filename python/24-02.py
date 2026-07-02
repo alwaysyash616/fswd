656ef2f75c4dffb50d65f37ffeebcbcc93976615
Yash Kushwaha <yash274602@gmail.com>
2026-07-02 07:33:15 +0530

    add A24-02 python

---
+++ python/24-02.py
@@ -0,0 +1,16 @@
+"""
+Assignment: 24
+Problem: 02
+Topic: list
+Description: Write a python script to calculate average of elements of a list.
+Date: 30-06-2026
+"""
+
+l1=[1,2,3,4,5]
+avg=sum(l1)/len(l1)
+print("Average:",avg)
+
+# print("Enter numbers separated by comma")
+# l1=[int(e) for e in input().split(',')]
+# avg=sum(l1)/len(l1)
+# print(avg)
