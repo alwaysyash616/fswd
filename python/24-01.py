6c7eed402325b413cdabae896d3656acc83ee53a
Yash Kushwaha <yash274602@gmail.com>
2026-07-02 07:32:28 +0530

    add A24-01 python

---
+++ python/24-01.py
@@ -0,0 +1,20 @@
+"""
+Assignment: 24
+Problem: 01
+Topic: list
+Description: Write a python script to calculate sum of elements of a list.
+Date: 30-06-2026
+"""
+
+l1=[1,3,5,7,9]
+sum=0
+for e in l1:
+    sum+=e
+print(sum)
+
+# built-ins
+# print(sum(l1))
+
+# print("Enter numbers separated by comma")
+# l1=[int(e) for e in input().split(',')]
+# print(sum(l1))
