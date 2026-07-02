e34f95ae8f8ca6231267e849473005987176b08c
Yash Kushwaha <yash274602@gmail.com>
2026-07-02 07:34:18 +0530

    add A24-04 python

---
+++ python/24-04.py
@@ -0,0 +1,16 @@
+"""
+Assignment: 24
+Problem: 04
+Topic: list
+Description: Write a python script to sort list elements in descending order.
+Date: 30-06-2026
+"""
+
+l1=[23,9,5,78,34,1,5,0,30]
+l1.sort(reverse=True)
+print(l1)
+
+# print("Enter numbers separated by comma")
+# l1=[int(e) for e in input().split(',')]
+# l1.sort(reverse=True)
+# print(l1)
