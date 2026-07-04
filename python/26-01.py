b9f755853d7736bcc9c92cc23455a13c47f96a5b
Yash Kushwaha <yash274602@gmail.com>
2026-07-04 17:55:17 +0530

    add A26-01 python

---
+++ python/26-01.py
@@ -0,0 +1,19 @@
+"""
+Assignment: 26
+Problem: 01
+Topic: str
+Description: Write a Python script to check if a given string has only alphabets in it.
+Date: 04-07-2026
+"""
+
+s='mysirg'
+print(s.isalpha())
+
+# for e in input("Enter some string: "):
+#     if e>='a' and e<='z' or e>='A' and e<='Z':
+#         pass
+#     else:
+#         print("String has at least one character which is not an alphabet")
+#         break
+# else:
+#     print("String contains only alphabets")
