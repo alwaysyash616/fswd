3029f2532b89e3284725a03da8c5afe7e32a0208
Yash Kushwaha <yash274602@gmail.com>
2026-07-04 18:55:49 +0530

    add A27-04 python

---
+++ python/27-04.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 27
+Problem: 04
+Topic: more on str
+Description: Write a Python script to transform a given string to uppercase.
+Date: 04-07-2026
+"""
+
+s=input("Enter some string: ").upper()
+print(s)
