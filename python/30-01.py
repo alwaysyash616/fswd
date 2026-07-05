63a0d909d6c1f948e4ab751698e97100dc9fb417
Yash Kushwaha <yash274602@gmail.com>
2026-07-05 16:51:43 +0530

    add A30-01 python

---
+++ python/30-01.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 30
+Problem: 01
+Topic: tuple
+Description: Write a Python script to print all distinct elements of a list. Use set to solve the problem.
+Date: 04-07-2026
+"""
+
+l1=[2,2,9,2,10,'abc','Vue.Js','Angular','React','abc']
+for x in set(l1):
+    print(x,sep=' ')
+print()
