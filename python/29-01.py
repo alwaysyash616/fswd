1acd6b82e9133e047b04f8078af0d2b87939a228
Yash Kushwaha <yash274602@gmail.com>
2026-07-05 15:59:01 +0530

    add A29-01 python

---
+++ python/29-01.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 29
+Problem: 01
+Topic: tuple
+Description: Write a Python script to create a tuple from a given list.
+Date: 04-07-2026
+"""
+
+l1=[100,'alwaysyash',3+4j,2.14]
+t1=tuple(l1)
+print(t1)
