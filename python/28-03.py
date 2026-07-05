de3b74506bdf5f7bc802ee865ce2c11e6a2efccd
Yash Kushwaha <yash274602@gmail.com>
2026-07-05 12:36:39 +0530

    add A28-03 python

---
+++ python/28-03.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 28
+Problem: 03
+Topic: list and str mixed
+Description: Write a Python script to sort a list of strings
+Date: 04-07-2026
+"""
+
+l1=['MySirG','VeoLMS','OpenAI','GCP','ChatGPT','YouTUBE']
+l1.sort()
+print(l1)
