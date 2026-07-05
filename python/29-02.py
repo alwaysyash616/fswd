1a3bdfa604a2f73d4da161657083c369373c1df1
Yash Kushwaha <yash274602@gmail.com>
2026-07-05 15:59:32 +0530

    add A29-02 python

---
+++ python/29-02.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 29
+Problem: 02
+Topic: tuple
+Description: Write a Python script to reverse a tuple.
+Date: 04-07-2026
+"""
+
+t1=(616,'alwaysyash',3.14,2.17,4+7j,)
+print(t1[::-1])
