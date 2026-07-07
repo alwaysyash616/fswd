5ac20b989c143c4299c11a266056bb037d949375
Yash Kushwaha <yash274602@gmail.com>
2026-07-07 15:58:15 +0530

    add A32-03 python

---
+++ python/32-03.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 32
+Problem: 03
+Topic: Functions-1
+Description: Write a python function to calculate average of three numbers. (TSRS)
+Date: 04-07-2026
+"""
+
+def average(a,b,c):
+    return (a+b+c)/3
+
+print("Enter three numbers")
+print(average(int(input()),int(input()),int(input())))
