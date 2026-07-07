4878e99f1216648d5c35185c4d4e704c15b15589
Yash Kushwaha <yash274602@gmail.com>
2026-07-07 15:59:59 +0530

    add A32-05 python

---
+++ python/32-05.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 32
+Problem: 05
+Topic: Functions-1
+Description: Write a python function to calculate volume of a cuboid. (TSRS)
+Date: 04-07-2026
+"""
+
+def volume(l,b,h):
+    return l*b*h
+
+print("Enter length, breadth and height:")
+print(volume(int(input()),int(input()),int(input())))
