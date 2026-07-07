6eff3a8f1f4654c22dc2ba78939828d5e2c78830
Yash Kushwaha <yash274602@gmail.com>
2026-07-07 15:57:47 +0530

    add A32-02 python

---
+++ python/32-02.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 32
+Problem: 02
+Topic: Functions-1
+Description: Write a python function to calculate area of a circle(TSRS)
+Date: 04-07-2026
+"""
+
+def area(r):
+    return 3.14*r**2
+
+print(area(int(input("Enter radius: "))))
