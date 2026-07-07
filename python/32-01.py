a3738a93e183eb9cec0081e8ae8250b467710ae4
Yash Kushwaha <yash274602@gmail.com>
2026-07-07 15:56:47 +0530

    add A32-01 python

---
+++ python/32-01.py
@@ -0,0 +1,12 @@
+"""
+Assignment: 32
+Problem: 01
+Topic: Functions-1
+Description: Write a python function to calculate sum of two numbers. (TSRS)
+Date: 04-07-2026
+"""
+
+def add(a,b):
+    return a+b
+
+print(add(3,4))
