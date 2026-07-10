940ae219cb7a37e73516f668806ad697038e0508
Yash Kushwaha <yash274602@gmail.com>
2026-07-10 18:31:14 +0530

    add A36-01 python

---
+++ python/36-01.py
@@ -0,0 +1,13 @@
+"""
+Assignment: 36
+Problem: 01
+Topic: Functions-4
+Description: Write a Python function to remove duplicate elements from a given list. (TSRS)
+Date: 04-07-2026
+"""
+
+def removeDuplicates(mylist):
+    return list(set(l1))
+
+l1=[2,3,5,'list',3+4j,4.6,3,'list',4.6]
+print(removeDuplicates(l1))
