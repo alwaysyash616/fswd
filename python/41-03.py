366cca1c082caa9ad44346501f5929e316a9d90d
Yash Kushwaha <yash274602@gmail.com>
2026-07-15 23:48:48 +0530

    add A41-03 python

---
+++ python/41-03.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 41
+Problem: 03
+Topic: lambda
+Description: Write a lambda expression to calculate area of a circle.
+Date: 15-07-2026
+"""
+
+area=lambda r:3.14*r**2
+print(area(int(input("Enter a number: "))))
