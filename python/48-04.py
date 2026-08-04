0d2251e8f93096065aef3112619ce9e72fe3cd70
Yash Kushwaha <yash274602@gmail.com>
2026-08-04 08:04:20 +0530

    add A48-04 python

---
+++ python/48-04.py
@@ -0,0 +1,11 @@
+"""
+Assignment: 48
+Problem: 04
+Topic: Exception Handling
+Description: When finally is executed?
+Date: 04-08-2026
+"""
+
+# always
+# Exception raised, not raised
+# Our Exception handler code executed, OR Python default Exception handler code executed
