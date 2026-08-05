11afc00effb1657f024ed90fc2229b1849aa5b17
Yash Kushwaha <yash274602@gmail.com>
2026-08-05 07:53:58 +0530

    add A48-05 python

---
+++ python/48-05.py
@@ -0,0 +1,10 @@
+"""
+Assignment: 48
+Problem: 05
+Topic: Exception Handling
+Description: Can we write try block without except clause?
+Date: 05-08-2026
+"""
+
+# Yes, if we use finally block in our code. Then, we have can decide not to use except block in our code.
+# Any one among 'except' OR 'finally' is mandatory.
